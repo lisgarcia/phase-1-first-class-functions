38bb1d424467df2ac9b7301935935a104e8fdc77
Elisabeth Garcia <lisgarciae@gmail.com>
2023-05-01 17:49:59 -0400

    redone

--- index.js
+++ index.js
@@ -1,31 +1,67 @@
-function Spy () {
-  console.log("This works")
+function receivesAFunction (spy) {
+    return spy()
 }
 
-function receivesAFunction (Spy) {
-    return Spy()
+function returnsANamedFunction() {
+    function innerFunction () {
+      console.log("spy")
+    }
+    return innerFunction
 }
-receivesAFunction(Spy)
 
 
-//
-function returnsANamedFunction () {
-  function innerFunction () {
-    console.log(Inner)
-  }
-  return innerFunction
+function returnsAnAnonymousFunction() {
+    return function () {
+      console.log('over')
+    }
 }
-returnsANamedFunction()
-//
+returnsAnAnonymousFunction()
 
 
 
-function returnsAnAnonymousFunction () {
-return function () {console.log("please")}
-}
-console.log(returnsAnAnonymousFunction())
 
-// function fn (name) {
-// let fn=returnsAnAnonymousFunction()
-//   return fn
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Spy () {
+//   console.log("This works")
 // }
+
+// function receivesAFunction (Spy) {
+//     return Spy()
+// }
+// receivesAFunction(Spy)
+
+
+// //
+// function returnsANamedFunction () {
+//   function innerFunction () {
+//     console.log('Inner')
+//   }
+//   return innerFunction
+// }
+// returnsANamedFunction()
+// //
+
+
+
+// function returnsAnAnonymousFunction () {
+// return function () {console.log("please")}
+// }
+// console.log(returnsAnAnonymousFunction())
+
+// // function fn (name) {
+// // let fn=returnsAnAnonymousFunction()
+// //   return fn
+// // }
